d41724248b34ba5e8f8b65f8f1c62f564b4fab65
zenghuasheng <zenghuasheng@ones.ai>
2024-07-10 18:22:25 +0800

    内置模板

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,63 +6,92 @@ import { useRouter } from 'vue-router'
 const router = useRouter()
 
 const logFile = ref(null)
-const logPattern = ref('^(\\S+) - - \\[(.*?)\\] "(.*?)" (\\d+) (\\d+) "(.*?)" "(.*?)" "(.*?)" "([\\d\\.]+)"$')
-const schemaFields = ref('ip_address:ID,timestamp:TEXT,endpoint:TEXT,status:NUMERIC,size:NUMERIC,referer:TEXT,user_agent:TEXT,request_ip:ID,response_time:NUMERIC')
-const formatFields = ref('def format_fields(match, schema_fields):\n' +
-    '    line_map = {}\n' +
-    '    for i, field in enumerate(schema_fields):\n' +
-    '        field_value = match.group(i + 1)\n' +
-    '        if schema_fields[field] == "NUMERIC":\n' +
-    '            # parsed_lines.append({field: int(field_value)})\n' +
-    '            # 尝试转为 int,如果失败，转为 float\n' +
-    '            try:\n' +
-    '                field_value = int(field_value)\n' +
-    '                line_map[field] = field_value\n' +
-    '            except ValueError:\n' +
-    '                field_value = float(field_value)\n' +
-    '                field_value = int(field_value * 1000)\n' +
-    '                line_map[field] = field_value\n' +
-    '        else:\n' +
-    '            line_map[field] = field_value\n' +
-    '    return line_map\n')
-const parse_example = ref(
-'project-api:'+
-    '\n\\[(.*?)\\] (\\d{4}/\\d{2}/\\d{2} \\d{2}:\\d{2}:\\d{2}) (\\d{2}/\\d{2} - \\d{2}:\\d{2}:\\d{2}) (\\d{3}) (\\d{1,}.\\d{1,})(m?s) (\\S+) (\\S+) (.*$)' +
-    '\nlog_level:ID,log_time1:TEXT,log_time2:TEXT,status:NUMERIC,response_time:NUMERIC,request_ip:ID,method:ID,endpoint:TEXT' +
-    '\ndef format_fields(match, schema_fields):\n' +
-    '    line_map = {}\n' +
-    '    for i, field in enumerate(schema_fields):\n' +
-    '        field_value = match.group(i + 1)\n' +
-    '        if schema_fields[field] == "NUMERIC":\n' +
-    '            # parsed_lines.append({field: int(field_value)})\n' +
-    '            # 尝试转为 int,如果失败，转为 float\n' +
-    '            if field == "response_time":\n' +
-    '                # 带有单位，比如 1.23ms 1.23s 1min2s\n' +
-    '                if "ms" in field_value:\n' +
-    '                    field_value = field_value.replace("ms", "")\n' +
-    '                    field_value = float(field_value)\n' +
-    '                    field_value = int(field_value)\n' +
-    '                elif "s" in field_value:\n' +
-    '                    field_value = field_value.replace("s", "")\n' +
-    '                    field_value = float(field_value)\n' +
-    '                    field_value = int(field_value * 1000)\n' +
-    '                elif "min" in field_value:\n' +
-    '                    field_value = field_value.replace("min", "")\n' +
-    '                    field_value = field_value.split("s")\n' +
-    '                    field_value = int(field_value[0]) * 60 + int(field_value[1])\n' +
-    '                line_map[field] = field_value\n' +
-    '            else:\n' +
-    '                try:\n' +
-    '                    field_value = int(field_value)\n' +
-    '                    line_map[field] = field_value\n' +
-    '                except ValueError:\n' +
-    '                    field_value = float(field_value)\n' +
-    '                    field_value = int(field_value * 1000)\n' +
-    '                    line_map[field] = field_value\n' +
-    '        else:\n' +
-    '            line_map[field] = field_value\n' +
-    '    return line_map\n'
-)
+const logPattern = ref('')
+const schemaFields = ref('')
+const formatFields = ref('')
+const parse_example = ('')
+
+
+const logTypeOptions = ref([
+  {
+    name: 'nginx',
+    logPattern: '^(\\S+) - - \\[(.*?)\\] "(.*?)" (\\d+) (\\d+) "(.*?)" "(.*?)" "(.*?)" "([\\d\\.]+)"$',
+    schemaFields: 'ip_address:ID,timestamp:TEXT,endpoint:TEXT,status:NUMERIC,size:NUMERIC,referer:TEXT,user_agent:TEXT,request_ip:ID,response_time:NUMERIC',
+    formatFields: `
+def format_fields(match, schema_fields):
+    line_map = {}
+    for i, field in enumerate(schema_fields):
+        field_value = match.group(i + 1)
+        if schema_fields[field] == "NUMERIC":
+            # parsed_lines.append({field: int(field_value)})
+            # 尝试转为 int,如果失败，转为 float
+            try:
+                field_value = int(field_value)
+                line_map[field] = field_value
+            except ValueError:
+                field_value = float(field_value)
+                field_value = int(field_value * 1000)
+                line_map[field] = field_value
+        else:
+            line_map[field] = field_value
+    return line_map
+    `
+  },
+  {
+    name: 'project-api',
+    logPattern: '\\[(.*?)\\] (\\d{4}/\\d{2}/\\d{2} \\d{2}:\\d{2}:\\d{2}) (\\d{2}/\\d{2} - \\d{2}:\\d{2}:\\d{2}) (\\d{3}) (\\S+) (\\S+) (\\S+) (.*$)',
+    schemaFields: 'log_level:ID,log_time1:TEXT,log_time2:TEXT,status:NUMERIC,response_time:NUMERIC,request_ip:ID,method:ID,endpoint:TEXT',
+    formatFields: `
+def format_fields(match, schema_fields):
+    line_map = {}
+    for i, field in enumerate(schema_fields):
+        field_value = match.group(i + 1)
+        if schema_fields[field] == "NUMERIC":
+            # parsed_lines.append({field: int(field_value)})
+            # 尝试转为 int,如果失败，转为 float
+            if field == "response_time":
+                # 带有单位，比如 1.23µs 1.23ms 1.23s 1min2s
+                if "µs" in field_value:
+                    field_value = field_value.replace("µs", "")
+                    field_value = float(field_value)
+                    field_value = int(field_value)
+                elif "ms" in field_value:
+                    field_value = field_value.replace("ms", "")
+                    field_value = float(field_value)
+                    field_value = int(field_value)
+                elif "s" in field_value:
+                    field_value = field_value.replace("s", "")
+                    field_value = float(field_value)
+                    field_value = int(field_value * 1000)
+                elif "min" in field_value:
+                    field_value = field_value.replace("min", "")
+                    field_value = field_value.split("s")
+                    field_value = int(field_value[0]) * 60 + int(field_value[1])
+                line_map[field] = field_value
+            else:
+                try:
+                    field_value = int(field_value)
+                    line_map[field] = field_value
+                except ValueError:
+                    field_value = float(field_value)
+                    field_value = int(field_value * 1000)
+                    line_map[field] = field_value
+        else:
+            line_map[field] = field_value
+    return line_map
+`
+  }
+])
+
+const selectedLogType = ref(0)
+const onLogTypeChange = () => {
+  const selectedOption = logTypeOptions.value[selectedLogType.value]
+  logPattern.value = selectedOption.logPattern
+  schemaFields.value = selectedOption.schemaFields
+  formatFields.value = selectedOption.formatFields
+}
+
+
 const md5Hash = ref('')
 const first100Parsed = ref(null)
 const error = ref(null)
@@ -73,16 +102,20 @@ const parsedLogList = ref([])
 
 // Fetch parsed log list from backend on component mount
 onMounted(async () => {
-  try {
-    const response = await fetch('/api/parsed-log-list')
-    if (!response.ok) {
-      throw new Error('Failed to fetch parsed log list')
-    }
-    parsedLogList.value = await response.json()
-  } catch (error) {
-    console.error('Error fetching parsed log list:', error)
-  }
+  fetchParsedLogs()
+  onLogTypeChange()
 })
+
+const fetchParsedLogs = () => {
+  axios.get('/api/parsed-log-list')
+      .then(response => {
+        parsedLogList.value = response.data
+      })
+      .catch(error => {
+        error.value = error.response.data.error || 'Failed to fetch parsed logs'
+      })
+}
+
 
 const handleFileUpload = (event) => {
   logFile.value = event.target.files[0]
@@ -155,18 +188,16 @@ const confirmParse = async () => {
       </ul>
     </div>
     <h3>解析日志</h3>
+    <select v-model="selectedLogType" @change="onLogTypeChange">
+      <option v-for="(option, index) in logTypeOptions" :key="index" :value="index">
+        {{ option.name }}
+      </option>
+    </select>
     <input type="file" @change="handleFileUpload" />
     <input type="text" v-model="logPattern" placeholder="Log Pattern" />
     <input type="text" v-model="schemaFields" placeholder="Schema Fields" />
     <textarea class="format-field-code" v-model="formatFields" placeholder="Format Fields Function" />
     <button @click="uploadLogFile">Upload</button>
-    <div>
-      <a href="https://regex101.com/" target="_blank">正则表达式网站</a>
-      <pre>
-        {{ parse_example }}
-      </pre>
-    </div>
-
     <div v-if="error" class="error">
       <pre>{{ error }}</pre>
     </div>
